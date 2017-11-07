29c8e7bf36c8db3e07613c5f5f14dc1560f09427
Mimi Scott <mimiscott94@gmail.com>
2017-11-07 11:04:00 -0500

    sub uri logging

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 
+app.use(function (req, res, next) {
+    //console.log(req.method);
+    //console.log(res.method); //undefined
+    console.log(req.method, req.originalUrl, res.statusCode);
+    next();
+    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+})
+
+app.use( function (req, res, next) {
+    console.log('you reached the ~special~ area');
+    next();
+})
+
+
 app.use(function(req, res, next) {
   console.log('Someome made a requst!');
   next();
@@ -14,7 +28,16 @@ app.get('/news', function(req, res, next) {
   res.send("Nothing good...");
 })
 
+app.get('/special/', function(req, res, next) {
+    res.send("This is the special area");
+  })
+
+app.get('/special/holidays', function(req, res, next) {
+  res.send("This is the special holiday area");
+})
+
 app.listen(3000, function() {
   // does something
   console.log("Bloop");
 })
+
